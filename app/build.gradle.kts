91a5281e8ea07a2b19683dac2d086594df0c4efb
JHY3066 <jkjjjfamily30@gmail.com>
2024-05-27 20:17:53 +0900

    search bar 생성

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,12 +36,13 @@ android {
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation("androidx.appcompat:appcompat:1.3.0")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.5.10")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/example/glowhackaton/MainActivity.kt
+++ app/src/main/java/com/example/glowhackaton/MainActivity.kt
@@ -1,20 +1,29 @@
 package com.example.glowhackaton
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.SearchView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var search_bar: SearchView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        search_bar = findViewById(R.id.search_bar)
+        search_bar.queryHint="시장을 검색하세요"
+
+        search_bar.setOnClickListener {
+            showKeyboard()
         }
+    }
+
+    private fun showKeyboard() {
+        val imm = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.toggleSoftInput(InputMethodManager.SHOW_FORCED, 0)
     }
 }
